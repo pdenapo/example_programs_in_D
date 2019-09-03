81e914eb044fe3ac39551b4383daee7792283fb6
Pablo De Nápoli <pdenapo@gmail.com>
2019-09-03 13:08:33 -0300

    Use my version of fixed.

--- serialize_fixed_to_Json/source/app.d
+++ serialize_fixed_to_Json/source/app.d
@@ -1,28 +1,47 @@
 
+// Fixed is a module for fixed point arithmetic in D
+
+import jaypha.fixed;
+import std.conv;
+
 import vibe.data.serialization;
 import vibe.data.json;
 import std.stdio;
 
-struct MyClass
+static struct S {
+	int value;
+
+	// dummy example implementations
+	string toString() const { return value.to!string(); }
+	static S fromString(string s) { return S(s.to!int()); }
+}
+
+alias number= Fixed!2;
+
+struct MyStruct
 {
-  int int_data;
-  string string_data;
-  bool boolean_data;
+  @optional string string_data;
+ number fixed_data;
 }
 
 void main()
-{	/* A json string with different types of data */
+{	
+	// Let's create a variable with 2 decimal places
+	Fixed!2 a = "2.5";
+	writeln("a=",a);
+	// We want to be sure that we are using serializable verion of fixed
+	//static assert(isStringSerializable!number);	
+	/* A json string with different types of data */
 	string my_json_string = "{\"int_data\":2,
 			          \"string_data\":\"hello world!\",
-			          \"boolean_data\":true }";
+			          \"boolean_data\":true, 
+			          \"fixed_data\":\"2.5\" }";
 	/* Let's convert it to a Json representation using vibe */
 	Json my_json = parseJsonString(my_json_string);
 	// vibe Json objects are nicely printed
 	writeln("my_json=",my_json);
 	// This function authomatically converts a Json object to a D object
-	MyClass my_object = deserializeJson!MyClass(my_json);
+	MyStruct my_object = deserializeJson!MyStruct(my_json);
 	// Let's check that our data are there...
 	writeln("string_data=",my_object.string_data);
-	writeln("int_data=",my_object.int_data);
-	writeln("boolean_data=",my_object.boolean_data);
 }
