1ee16c35f17cd48b4de3cefa173f122078073b06
Pablo De Nápoli <pdenapo@gmail.com>
2019-09-03 15:52:07 -0300

    Fixed the test program to use the fromString method

--- serialize_fixed/source/app.d
+++ serialize_fixed/source/app.d
@@ -8,14 +8,6 @@ import vibe.data.serialization;
 import vibe.data.json;
 import std.stdio;
 
-static struct S {
-	int value;
-
-	// dummy example implementations
-	string toString() const { return value.to!string(); }
-	static S fromString(string s) { return S(s.to!int()); }
-}
-
 alias number= Fixed!2;
 
 struct MyStruct
@@ -26,11 +18,12 @@ struct MyStruct
 
 void main()
 {	
-	// Let's create a variable with 2 decimal places
-	Fixed!2 a = "2.5";
-	writeln("a=",a);
 	// We want to be sure that we are using serializable verion of fixed
-	//static assert(isStringSerializable!number);	
+	static assert(isStringSerializable!number);
+	// Let's create a variable with 2 decimal places using the new fromString method
+	Fixed!2 a = Fixed!2.fromString("2.5");
+	writeln("a=",a);
+	
 	/* A json string with different types of data */
 	string my_json_string = "{\"int_data\":2,
 			          \"string_data\":\"hello world!\",
@@ -44,4 +37,5 @@ void main()
 	MyStruct my_object = deserializeJson!MyStruct(my_json);
 	// Let's check that our data are there...
 	writeln("string_data=",my_object.string_data);
+	writeln("fixed_data=",my_object.fixed_data);
 }
