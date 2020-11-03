ee19ba287765b5e55565cd31647fe9a557c0bbf0
Pablo De Nápoli <pdenapo@gmail.com>
2020-11-03 01:38:32 -0300

    Added Lua console.

---
+++ lua_console/source/app.d
@@ -0,0 +1,57 @@
+// A nice video explaining how to embed Lua into a C++ program
+// This can be easily addapted to D
+//  https://www.youtube.com/watch?v=4l5HdmPoynw
+
+import std.stdio;
+import std.string;
+import core.stdc.stdio: printf;
+// The module to use Lua
+import  bindbc.lua;
+
+import gnu.readline;
+
+// We want to use a function not declared in the module gnu.readline
+// declared in the header readline/history.h
+extern (C) 
+{
+  void add_history (const char *);
+}
+
+// An example from LuaD documentation
+int printTimes(int times, const(char)[] message)
+{
+    for(int i = 0; i <= times; i++)
+        writeln(message);
+    return times;
+}
+
+void main()
+{
+	writeln("Lua console using LuaD y GNU Readline");
+	lua_State* L= luaL_newstate();
+	// cargamos las librerías estándard de Lua
+	luaL_openlibs(L);
+    
+	while (true) 
+	{
+		char* line = readline (">");
+		if (!line)
+        	break; 	
+		add_history (line);
+		int r = luaL_dostring(L,line);
+
+		// if r is 0, the command was excecuted successfully.
+		// Otherwise, we print the error message.
+		
+		if (r != 0)
+		{
+			writeln("r=",r);
+			auto error_message= lua_tostring(L,-1);
+
+			// note that we can call the standard C printf 
+
+			printf("%s \n",error_message);
+		}
+	} /* end of while loop */
+}
+
