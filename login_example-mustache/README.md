b1c30c2972cd4cc235d7a2c91e08b152f2f2da7f
Pablo De Nápoli <pdenapo@gmail.com>
2021-02-21 13:01:38 -0300

    Nicer version of the login example using mustache and bootstrap.

---
+++ login_example-mustache/README.md
@@ -0,0 +1,6 @@
+
+This is a more elaborated version of the login example. It uses mustache
+instead of diet templates (the built-in templates in vibe-d).
+
+This allows us to use html syntax in the templates. Moreover, we use
+bootstrap to have a nicer look.
---
+++ login_example-mustache/source/app.d
@@ -0,0 +1,116 @@
+
+// Author: Pablo De Nápoli <pdenapo@gmail.com>
+// License: MIT
+
+import vibe.vibe;
+import vibe.web.auth;
+import vibe.web.web;
+import vibe.http.server;
+import mustache;
+
+MustacheEngine!(string) mustacheEngine;
+
+
+// In a real application the user credentials should not be hard coded on the source!
+
+const string required_username= "user";
+const string required_password= "secret";
+
+static struct WebAuthInfo {
+	string userName;
+ }
+
+
+@requiresAuth
+static class Web_Interface 
+{
+    private {
+		// stored in the session store
+	SessionVar!(bool, "authenticated") user_authenticated;
+	SessionVar!(string, "user_name") user_name;
+	SessionVar!(string, "user_password") user_password;
+	}
+	
+
+	@safe @noRoute WebAuthInfo authenticate(scope HTTPServerRequest req, scope HTTPServerResponse res)
+	{
+	  if (!user_authenticated) 
+		throw new HTTPStatusException(HTTPStatus.unauthorized);
+	  else 
+		return WebAuthInfo(user_name);
+	}
+	
+   // GET /
+   @noAuth void index()
+	{
+
+	auto mustacheContext = new MustacheEngine!(string).Context;
+	mustacheEngine.path  = "./views";
+   	mustacheEngine.level = MustacheEngine!string.CacheLevel.no;
+	mustacheContext["program_name"]= "Login example";
+	mustacheContext["description"]="Example of a minimal login application using vibe-d";
+
+	if (!user_authenticated)
+		{
+			string login_html=mustacheEngine.render("login", mustacheContext);
+			response.writeBody(login_html,"text/html");
+		}
+	else 
+		{
+			string private_html=mustacheEngine.render("private", mustacheContext);
+			response.writeBody(private_html,"text/html");
+		}
+	}
+	
+
+    @headerParam("username", "username") @headerParam("password", "password")
+    @noAuth @path("/login") 
+    void postLogin(string username, string password)
+	{
+	logInfo("Comenzamos el método login username=" ~ username ~ ",password=" ~ password);
+	bool chequeo_de_credenciales = username == required_username && 
+											   password == required_password ;
+	logInfo("chequeo_de_crenciales=" ~ to!string(chequeo_de_credenciales));
+	if (!chequeo_de_credenciales)
+		throw new HTTPStatusException(HTTPStatus.unauthorized,"Invalid user name or password.");
+	user_authenticated = true;
+	user_name = username;
+	user_password =  password;
+	
+	redirect("/");
+   }
+   
+
+    @anyAuth @path("/logout") 
+	void getLogout()
+	{
+		user_authenticated = false;
+		terminateSession();
+		redirect("/");
+	}
+
+} 
+
+
+void main()
+{
+	auto settings = new HTTPServerSettings;
+	settings.port = 8080;
+	settings.bindAddresses = ["127.0.0.1"];
+	settings.sessionStore = new MemorySessionStore;
+	
+	auto router = new URLRouter;
+    router.registerWebInterface(new Web_Interface);
+	
+	// sirve los archivos estáticos del directorio public_html
+	router.get("*", serveStaticFiles("public_html/"));
+
+
+
+
+	listenHTTP(settings, router);
+
+	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
+	runApplication();
+}
+
